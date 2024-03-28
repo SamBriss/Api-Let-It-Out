258da6a66c5ff2fddd2b769d344d59d85dc34f78
carolprian <a20300826@ceti.mx>
2024-03-27 19:03:25 -0600

    fix a little error

--- Api-Let-it-Out/src/main/java/com/apiLetItOut/Api/repository/InformativeArticle.java
+++ Api-Let-it-Out/src/main/java/com/apiLetItOut/Api/repository/InformativeArticle.java
@@ -2,10 +2,10 @@ package com.apiLetItOut.Api.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
+/*
 @Repository
 public interface InformativeArticle extends CrudRepository <InformativeArticle, Integer> {
 
     
 
-}
+}*/
